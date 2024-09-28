0415b93fe68e0fc66997ac15b6211ae8da4b574d
barathbalasubramanian <105224717+barathbalasubramanian@users.noreply.github.com>
2024-09-28 12:06:45 +0530

    Deploy Err ..

--- components/About.js
+++ components/About.js
@@ -1,6 +1,7 @@
-import React from 'react'
-import Styles from './about.module.css'
+import React from 'react';
+import Styles from './about.module.css';
 import Image from 'next/image';
+
 function About() {
   return (
     <div className={Styles.lgui}>
@@ -8,27 +9,42 @@ function About() {
         <div className={`${Styles.container1} flex flex-col gap-4`}>
           <div>
             <div className={`px-2 ${Styles.about} mt-4 text-6xl`}>WHO WE ARE</div>
-            <div className={`${Styles.aboutcon} capitalize`}>At Anthill Networks, we specialize in <span className='bg-[rgba(150,149,150,40%)]'>transforming 
-            the ideas of non-tech founders into fully functional SaaS products.</span> From concept to launch, we handle all the technical details so you can focus on your vision</div>
+            <div className={`${Styles.aboutcon} capitalize`}>
+              At Anthill Networks, we specialize in{' '}
+              <span className='bg-[rgba(150,149,150,40%)]'>
+                transforming the ideas of non-tech founders into fully functional SaaS products.
+              </span>{' '}
+              From concept to launch, we handle all the technical details so you can focus on your vision
+            </div>
           </div>
           <div className={Styles.Img}>
-            <div><Image src="/assets/img2.svg" width={100} height={100} className={Styles.img1} alt='Img1'/></div>
+            <div>
+              <Image src="/assets/img2.svg" width={100} height={100} className={Styles.img1} alt="Img1" />
+            </div>
           </div>
         </div>
         {/* <div className={Styles.container2}>Personalized approach each project is a unique story. </div> */}
         <div className={`${Styles.container3} flex flex-col gap-4`}>
           <div className={Styles.Img}>
-            <div><Image src="/assets/img1.svg" width={100} height={100} className={Styles.img1} alt='Img1'/></div>
+            <div>
+              <Image src="/assets/img1.svg" width={100} height={100} className={Styles.img1} alt="Img1" />
+            </div>
           </div>
           <div>
             <div className={`px-2 ${Styles.ans} flex items-start justify-stretch gap-4`}>
-              <div className='text-2xl text-[var(--color-navy)]'>
+              <div className="text-2xl text-[var(--color-navy)]">
                 <div>A</div>
                 <div>N</div>
               </div>
-              <div className='text-xl w-[60%]'>Personalized approach each project is a unique story. </div>
+              <div className="text-xl w-[60%]">Personalized approach each project is a unique story.</div>
             </div>
-            <div className={`${Styles.aboutcon}`}>Our goal is to offer <span className='bg-[rgba(150,149,150,40%)]'> reliable and affordable solutions, making us the perfect alternative to freelancers or high-cost agencies.</span> We don't just build software - we craft the technology that powers your business.</div>
+            <div className={`${Styles.aboutcon}`}>
+              Our goal is to offer{' '}
+              <span className="bg-[rgba(150,149,150,40%)]">
+                reliable and affordable solutions, making us the perfect alternative to freelancers or high-cost agencies.
+              </span>{' '}
+              We don&#39;t just build software - we craft the technology that powers your business.
+            </div>
           </div>
         </div>
       </div>
@@ -36,34 +52,47 @@ function About() {
         <div className={`${Styles.container1} flex flex-col gap-4`}>
           <div>
             <div className={`px-2 ${Styles.about} mt-4`}>WHO WE ARE</div>
-            <div className={`${Styles.aboutcon}`}>At Anthill Networks, we specialize in <span className='bg-[rgba(150,149,150,40%)]'>transforming 
-            the ideas of non-tech founders into fully functional SaaS products.</span> From concept to launch, we handle all the technical details so you can focus on your vision</div>
+            <div className={`${Styles.aboutcon}`}>
+              At Anthill Networks, we specialize in{' '}
+              <span className="bg-[rgba(150,149,150,40%)]">
+                transforming the ideas of non-tech founders into fully functional SaaS products.
+              </span>{' '}
+              From concept to launch, we handle all the technical details so you can focus on your vision
+            </div>
           </div>
           <div className={Styles.Img}>
             <div>
-              <Image src="/assets/img1.svg" width={100} height={100} className={Styles.img1} alt='Img1'/>
-              <div className={Styles.container2}>Personalized approach each project is a unique story. </div>
+              <Image src="/assets/img1.svg" width={100} height={100} className={Styles.img1} alt="Img1" />
+              <div className={Styles.container2}>Personalized approach each project is a unique story.</div>
             </div>
           </div>
         </div>
         <div className={`${Styles.container3} flex flex-col gap-4`}>
           <div className={Styles.Img}>
-            <div><Image src="/assets/img2.svg" width={100} height={100} className={Styles.img1} alt='Img1'/></div>
+            <div>
+              <Image src="/assets/img2.svg" width={100} height={100} className={Styles.img1} alt="Img1" />
+            </div>
           </div>
           <div>
-          <div className={`px-2 ${Styles.ans} flex items-start justify-stretch gap-4 max-md:items-center`}>
-              <div className='text-2xl text-[var(--color-navy)]'>
+            <div className={`px-2 ${Styles.ans} flex items-start justify-stretch gap-4 max-md:items-center`}>
+              <div className="text-2xl text-[var(--color-navy)]">
                 <div>A</div>
                 <div>N</div>
               </div>
-              <div className='md:text-xl max-md:text-[12px] max-md:w-[100%] w-[60%]'>Personalized approach each project is a unique story. </div>
+              <div className="md:text-xl max-md:text-[12px] max-md:w-[100%] w-[60%]">Personalized approach each project is a unique story.</div>
             </div>
-            <div className={`${Styles.aboutcon}`}>Our goal is to offer <span className='bg-[rgba(150,149,150,40%)]'> reliable and affordable solutions, making us the perfect alternative to freelancers or high-cost agencies.</span> We don't just build software - we craft the technology that powers your business.</div>
+            <div className={`${Styles.aboutcon}`}>
+              Our goal is to offer{' '}
+              <span className="bg-[rgba(150,149,150,40%)]">
+                reliable and affordable solutions, making us the perfect alternative to freelancers or high-cost agencies.
+              </span>{' '}
+              We don&#39;t just build software - we craft the technology that powers your business.
+            </div>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default About;
--- components/Header.js
+++ components/Header.js
@@ -1,116 +1,115 @@
-"use client"
-import React from 'react'
-import Styles from './header.module.css'
-import Image from 'next/image'
+"use client";
+import React from 'react';
+import Styles from './header.module.css';
+import Image from 'next/image';
 
 function Header() {
   return (
     <div>
-        <div className={Styles.mainCon}>
-            <div className={`${Styles.container} items-center`}>
-                <div className='hidden md:block'><Image className={Styles.logo} src="/assets/logo.svg" alt='Logo' width={100} height={100}/></div>
-                <div className='flex md:hidden'><img src="/assets/logo.svg" alt=""  className='w-[10em]'/></div>
-                <div className={`flex gap-8 ${Styles.links}`}>
-                    <div className='flex items-center justify-center gap-2 cursor-pointer' onClick={()=>{console.log("")}}>
-                        <div>
-                            {/* <Image className={Styles.img} src="/assets/rightarrow.svg" alt='Logo' width={100} height={100}/> */}
-                        </div>
-                        <div className={Styles.ids}>ABOUT</div>
-                    </div>
-                    <div className='flex items-center justify-center gap-2 cursor-pointer' onClick={()=>{console.log("")}}>
-                        <div>
-                            {/* <Image className={Styles.img} src="/assets/rightarrow.svg" alt='Logo' width={100} height={100}/> */}
-                        </div>
-                        <div className={Styles.ids}>WORKS</div>
-                    </div>
-                    <div className='flex items-center justify-center gap-2 cursor-pointer' onClick={()=>{console.log("")}}>
-                        <div>
-                            {/* <Image className={Styles.img} src="/assets/rightarrow.svg" alt='Logo' width={100} height={100}/> */}
-                        </div>
-                        <div className={Styles.ids}>SERVICES</div>
-                    </div>
-                    <div className='flex items-center justify-center gap-2 cursor-pointer' onClick={()=>{console.log("")}}>
-                        <div>
-                            {/* <Image className={Styles.img} src="/assets/rightarrow.svg" alt='Logo' width={100} height={100}/> */}
-                        </div>
-                        <div className={Styles.ids}>CONTACT</div>
-                    </div>
-                </div>
-                <div className='text-black px-6 py-1 max-md:px-1 max-md:text-[10px] capitalize border border-black font-medium'>
-                    Let's Build Together
-                </div>
-                <div className='max-md:flex hidden'>
-                    <img src="/assets/hamburger.svg" alt="" />
-                </div>
+      <div className={Styles.mainCon}>
+        <div className={`${Styles.container} items-center`}>
+          <div className="hidden md:block">
+            <Image className={Styles.logo} src="/assets/logo.svg" alt="Logo" width={100} height={100} />
+          </div>
+          <div className="flex md:hidden">
+            <Image src="/assets/logo.svg" alt="Logo" width={160} height={160} />
+          </div>
+          <div className={`flex gap-8 ${Styles.links}`}>
+            <div className="flex items-center justify-center gap-2 cursor-pointer" onClick={() => { console.log('') }}>
+              <div>{/* <Image className={Styles.img} src="/assets/rightarrow.svg" alt="Logo" width={100} height={100} /> */}</div>
+              <div className={Styles.ids}>ABOUT</div>
             </div>
-        </div>
-
-        <div className={Styles.mainCon1}>
-            <div className={Styles.crtDiv}>
-                <div className={Styles.crt}>
-                    <div className={`${Styles.creative} ${Styles.creative1}`} style={{alignSelf:"start",alignItems:'start'}}>
-                        <div className={Styles.name}>YOUR</div>
-                        <div className={Styles.name1}>VISION</div>
-                    </div>
-                    <div>
-                        <div className={Styles.creativecon}>Your Trusted Partner in Turning Visionary Ideas into Reality </div>
-                    </div>
-                </div>
+            <div className="flex items-center justify-center gap-2 cursor-pointer" onClick={() => { console.log('') }}>
+              <div>{/* <Image className={Styles.img} src="/assets/rightarrow.svg" alt="Logo" width={100} height={100} /> */}</div>
+              <div className={Styles.ids}>WORKS</div>
             </div>
-        </div>
-
-        <div className={Styles.mainCon2}>
-            <div className={Styles.crtDiv}>
-                <div className={Styles.crt}>
-                    <div>
-                        <div className={`${Styles.creativecon1} ${Styles.creative2} ${Styles.aipart}`}></div>
-                    </div>
-                    <div className={`${Styles.ai} text-xl font-medium`}>
-                        TRANSFORMING IDEAS INTO DIGITAL SUCCESS
-                    </div>
-                    <div className={`${Styles.creative} ${Styles.creativeai}`}></div>
-                </div>
+            <div className="flex items-center justify-center gap-2 cursor-pointer" onClick={() => { console.log('') }}>
+              <div>{/* <Image className={Styles.img} src="/assets/rightarrow.svg" alt="Logo" width={100} height={100} /> */}</div>
+              <div className={Styles.ids}>SERVICES</div>
             </div>
-        </div>
-
-        <div className={Styles.mainCon1}>
-            <div className={Styles.crtDiv}>
-                <div className={Styles.crt}>
-                    <div>
-                        <div className={`${Styles.creativecon1} ${Styles.creative2}`}>We provide comprehensive SaaS solutions from concept to launch</div>
-                    </div>
-                    <div className={Styles.creative} style={{alignSelf:"start",alignItems:'start'}}>
-                        <div className={Styles.name1}>OUR</div>
-                        <div className={Styles.name}>EXPERTISE</div>
-                    </div>
-                </div>
+            <div className="flex items-center justify-center gap-2 cursor-pointer" onClick={() => { console.log('') }}>
+              <div>{/* <Image className={Styles.img} src="/assets/rightarrow.svg" alt="Logo" width={100} height={100} /> */}</div>
+              <div className={Styles.ids}>CONTACT</div>
             </div>
+          </div>
+          <div className="text-black px-6 py-1 max-md:px-1 max-md:text-[10px] capitalize border border-black font-medium">
+            Let&#39;s Build Together
+          </div>
+          <div className="max-md:flex hidden">
+            <Image src="/assets/hamburger.svg" alt="Hamburger Menu" width={50} height={50} />
+          </div>
         </div>
+      </div>
 
-        <div className={Styles.Homemobile}>
-            <div className='flex justify-between gap-2 border border-x-gray-400'>
-                <div className='min-w-[33%] max-w-[33%] max-md:min-h-[20em] min-h-[25em] flex flex-col items-start justify-between border border-r-gray-400'>
-                    <div className='h-[50%] px-4 border-b border-b-gray-400 w-full' style={{fontSize:"24px"}}>
-                        <div className={Styles.name}>YOUR</div>
-                        <div className={Styles.name1}>VISION</div>
-                    </div>
-                    <div  className='max-md:text-[12px] text-[var(--color-navy)] font-medium pb-4 px-2'>We provide comprehensive SaaS solutions from concept to launch</div>
-                </div>
-                <div className='max-md:text-[12px] min-w-[25%] max-w-[25%] max-md:min-h-[20em] min-h-[25em] text-[var(--color-navy)] font-medium flex flex-col items-center justify-center'>TRANSFORMING IDEAS INTO DIGITAL SUCCESS</div>
-                <div className='min-w-[35%] max-w-[35%] max-md:min-h-[20em] min-h-[25em] flex flex-col items-start justify-between border border-l-gray-400'>
-                    <div className='max-md:text-[12px] text-[var(--color-navy)]  border-b border-b-gray-400 w-full font-medium h-[50%] px-4'> 
-                        Your Trusted Partner in Turning Visionary Ideas into Reality
-                    </div>
-                    <div style={{fontSize:"25px !important"}} className='pb-4 pl-2'>
-                        <div className={Styles.name1}>OUR</div>
-                        <div className={Styles.name}>EXPERTISE</div>
-                    </div>
-                </div>
+      <div className={Styles.mainCon1}>
+        <div className={Styles.crtDiv}>
+          <div className={Styles.crt}>
+            <div className={`${Styles.creative} ${Styles.creative1}`} style={{ alignSelf: 'start', alignItems: 'start' }}>
+              <div className={Styles.name}>YOUR</div>
+              <div className={Styles.name1}>VISION</div>
             </div>
+            <div>
+              <div className={Styles.creativecon}>Your Trusted Partner in Turning Visionary Ideas into Reality</div>
+            </div>
+          </div>
         </div>
+      </div>
 
+      <div className={Styles.mainCon2}>
+        <div className={Styles.crtDiv}>
+          <div className={Styles.crt}>
+            <div>
+              <div className={`${Styles.creativecon1} ${Styles.creative2} ${Styles.aipart}`}></div>
+            </div>
+            <div className={`${Styles.ai} text-xl font-medium`}>TRANSFORMING IDEAS INTO DIGITAL SUCCESS</div>
+            <div className={`${Styles.creative} ${Styles.creativeai}`}></div>
+          </div>
+        </div>
+      </div>
+
+      <div className={Styles.mainCon1}>
+        <div className={Styles.crtDiv}>
+          <div className={Styles.crt}>
+            <div>
+              <div className={`${Styles.creativecon1} ${Styles.creative2}`}>
+                We provide comprehensive SaaS solutions from concept to launch
+              </div>
+            </div>
+            <div className={Styles.creative} style={{ alignSelf: 'start', alignItems: 'start' }}>
+              <div className={Styles.name1}>OUR</div>
+              <div className={Styles.name}>EXPERTISE</div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className={Styles.Homemobile}>
+        <div className="flex justify-between gap-2 border border-x-gray-400">
+          <div className="min-w-[33%] max-w-[33%] max-md:min-h-[20em] min-h-[25em] flex flex-col items-start justify-between border border-r-gray-400">
+            <div className="h-[50%] px-4 border-b border-b-gray-400 w-full" style={{ fontSize: '24px' }}>
+              <div className={Styles.name}>YOUR</div>
+              <div className={Styles.name1}>VISION</div>
+            </div>
+            <div className="max-md:text-[12px] text-[var(--color-navy)] font-medium pb-4 px-2">
+              We provide comprehensive SaaS solutions from concept to launch
+            </div>
+          </div>
+          <div className="max-md:text-[12px] min-w-[25%] max-w-[25%] max-md:min-h-[20em] min-h-[25em] text-[var(--color-navy)] font-medium flex flex-col items-center justify-center">
+            TRANSFORMING IDEAS INTO DIGITAL SUCCESS
+          </div>
+          <div className="min-w-[35%] max-w-[35%] max-md:min-h-[20em] min-h-[25em] flex flex-col items-start justify-between border border-l-gray-400">
+            <div className="max-md:text-[12px] text-[var(--color-navy)] border-b border-b-gray-400 w-full font-medium h-[50%] px-4">
+              Your Trusted Partner in Turning Visionary Ideas into Reality
+            </div>
+            <div style={{ fontSize: '25px !important' }} className="pb-4 pl-2">
+              <div className={Styles.name1}>OUR</div>
+              <div className={Styles.name}>EXPERTISE</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
