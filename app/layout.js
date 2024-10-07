5e804720e7495999cdc3089c31f8dcc97b9eaf1f
barathbalasubramanian <105224717+barathbalasubramanian@users.noreply.github.com>
2024-10-07 17:40:44 +0530

    Logo ..

--- app/layout.js
+++ app/layout.js
@@ -10,6 +10,8 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=League+Gothic:wght@400&display=swap" />
       <link href="https://fonts.googleapis.com/css2?family=Unbounded:wght@400&display=swap" rel="stylesheet"/>
+      {/* Logo link */}
+      <link rel="icon" href="/assets/logo2.svg" />
       <body>{children}</body>
     </html>
   );
